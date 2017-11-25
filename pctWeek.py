7b8d3dc514793b71ef1e0b863d013fbb744edd9a
ming <591177@qq.com>
2017-11-25 14:00:16 +0800

    'fixbugForloopToday'

--- pctWeek.py
+++ pctWeek.py
@@ -70,45 +70,46 @@ for i in range(8):
     days.insert(0, today-datetime.timedelta(i))
 
 for i, day in enumerate(days):
-    end = day + datetime.timedelta(1)
-    saleQuerySql = '''
-    SELECT {} FROM panda.`odi_order` oo
-    LEFT JOIN panda.`aci_user_info` aui
-    ON oo.`user_id` = aui.`user_id`
-    WHERE oo.`order_status` IN (1,2,4,5)
-    AND oo.`order_type` IN (1,2)
-    AND oo.`pay_at` > '{}'
-    AND oo.`pay_at` < '{}'
-    '''
+    if day != today:
+        end = day + datetime.timedelta(1)
+        saleQuerySql = '''
+        SELECT {} FROM panda.`odi_order` oo
+        LEFT JOIN panda.`aci_user_info` aui
+        ON oo.`user_id` = aui.`user_id`
+        WHERE oo.`order_status` IN (1,2,4,5)
+        AND oo.`order_type` IN (1,2)
+        AND oo.`pay_at` > '{}'
+        AND oo.`pay_at` < '{}'
+        '''
 
-    count = 'COUNT(1)'
-    scur.execute(saleQuerySql.format(count, day.strftime(dateFormat), end.strftime(dateFormat)))
-    result = scur.fetchone()
-    saleCount = result[0]
+        count = 'COUNT(1)'
+        scur.execute(saleQuerySql.format(count, day.strftime(dateFormat), end.strftime(dateFormat)))
+        result = scur.fetchone()
+        saleCount = result[0]
 
-    amount = 'SUM(oo.`total_amount`)'
-    scur.execute(saleQuerySql.format(amount, day.strftime(dateFormat), end.strftime(dateFormat)))
-    result = scur.fetchone()
-    saleAmount = result[0]
+        amount = 'SUM(oo.`total_amount`)'
+        scur.execute(saleQuerySql.format(amount, day.strftime(dateFormat), end.strftime(dateFormat)))
+        result = scur.fetchone()
+        saleAmount = result[0]
 
-    uvSql = '''
-    select ip from panda.`boss_api_info` bai
-    where bai.`created_at` = '{}'
-    '''
-    scur.execute(uvSql.format(day.strftime(dateFormat)))
-    result = scur.fetchone()
-    uv = result[0]
+        uvSql = '''
+        select ip from panda.`boss_api_info` bai
+        where bai.`created_at` = '{}'
+        '''
+        scur.execute(uvSql.format(day.strftime(dateFormat)))
+        result = scur.fetchone()
+        uv = result[0]
 
-    pct = decimal.Decimal('%.2f' % (saleAmount / saleCount))
-    transaction = '%.2f' % (saleAmount / uv / pct * 100)
+        pct = decimal.Decimal('%.2f' % (saleAmount / saleCount))
+        transaction = '%.2f' % (saleAmount / uv / pct * 100)
 
-    sheet.write(0, i+4, day.strftime(dateFormat))
-    sheet.write(1, i+4, day.strftime('%A'))
-    sheet.write(2, i+4, saleAmount)
-    sheet.write(3, i+4, saleCount)
-    sheet.write(4, i+4, uv)
-    sheet.write(5, i+4, transaction+'%')
-    sheet.write(6, i+4, pct)
+        sheet.write(0, i+4, day.strftime(dateFormat))
+        sheet.write(1, i+4, day.strftime('%A'))
+        sheet.write(2, i+4, saleAmount)
+        sheet.write(3, i+4, saleCount)
+        sheet.write(4, i+4, uv)
+        sheet.write(5, i+4, transaction+'%')
+        sheet.write(6, i+4, pct)
 
 path = cf.get('path', 'path')
 wb.save(path + today.strftime(dateFormat) + 'weekpct.xls')
