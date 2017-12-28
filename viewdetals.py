d76d14135242d7d7d6226e15b32f5a4902cc6250
ming <591177@qq.com>
2017-12-28 16:04:49 +0800

    viewdetails.py

---
+++ viewdetals.py
@@ -0,0 +1,51 @@
+import redis
+import config as conf
+import pymysql as db
+import xlwt
+
+cf = conf.product
+conn = db.connect(host=cf['host'], user=cf['user'], passwd=cf['pass'], db=cf['db'], port=cf['port'], charset=conf.char)
+cur = conn.cursor()
+pool = redis.ConnectionPool(host='118.190.149.162', port=36379)
+r = redis.StrictRedis(connection_pool=pool, decode_responses=True)
+workbook = xlwt.Workbook()
+prefix = 'count_product_detail_key:'
+
+product_sql = '''
+SELECT oo.product_id,pp.product_name,date(oo.pay_at) FROM panda.`odi_order` oo
+left join panda.pdi_product pp 
+on oo.product_id = pp.product_id
+WHERE oo.order_status IN (1,2,4,5)
+AND oo.order_type IN (1,2)
+AND oo.pay_at > '{}'
+AND oo.pay_at < '{}'
+'''
+cur.execute(product_sql.format('2016-1-1', '2017-1-1'))
+result = cur.fetchall()
+sheet = workbook.add_sheet('2016')
+sheet.write(0, 0, '销售日期')
+sheet.write(0, 1, '产品ID')
+sheet.write(0, 2, '产品名称')
+sheet.write(0, 3, '访问次数')
+for i, res in enumerate(result):
+    sheet.write(i+1, 0, str(res[2]))
+    sheet.write(i+1, 1, r[0])
+    sheet.write(i+1, 2, r[1])
+    sheet.write(i+1, 3, r.get(prefix+str(r[0])))
+
+cur.execute(product_sql.format('2017-1-1', 'date(now())'))
+result = cur.fetchall()
+sheet = workbook.add_sheet('2017')
+sheet.write(0, 0, '销售日期')
+sheet.write(0, 1, '产品ID')
+sheet.write(0, 2, '产品名称')
+sheet.write(0, 3, '访问次数')
+for i, res in enumerate(result):
+    sheet.write(i+1, 0, str(res[2]))
+    sheet.write(i+1, 1, r[0])
+    sheet.write(i+1, 2, r[1])
+    sheet.write(i+1, 3, r.get(prefix+str(r[0])))
+
+workbook.save('view.xls')
+cur.close()
+conn.close()
