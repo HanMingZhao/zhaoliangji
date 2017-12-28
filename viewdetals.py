dccc10450674137e988ee981148041935b2aeab1
ming <591177@qq.com>
2017-12-28 16:26:38 +0800

    fixbug

--- viewdetals.py
+++ viewdetals.py
@@ -34,9 +34,12 @@ for i, res in enumerate(result):
     sheet.write(i+1, 0, str(res[2]))
     sheet.write(i+1, 1, r[0])
     sheet.write(i+1, 2, r[1])
-    sheet.write(i+1, 3, r.get(prefix+str(r[0])))
+    try:
+        sheet.write(i+1, 3, r.get(prefix+str(r[0])))
+    finally:
+        sheet.write(i+1, 3, 0)
 print('2017sqlscan...', time.time()-start)
-cur.execute(product_sql.format('2017-1-1', 'date(now())'))
+cur.execute(product_sql.format('2017-1-1', '2018-1-1'))
 result = cur.fetchall()
 sheet = workbook.add_sheet('2017')
 sheet.write(0, 0, '销售日期')
@@ -48,7 +51,10 @@ for i, res in enumerate(result):
     sheet.write(i+1, 0, str(res[2]))
     sheet.write(i+1, 1, r[0])
     sheet.write(i+1, 2, r[1])
-    sheet.write(i+1, 3, r.get(prefix+str(r[0])))
+    try:
+        sheet.write(i+1, 3, r.get(prefix+str(r[0])))
+    finally:
+        sheet.write(i+1, 3, 0)
 
 workbook.save('view.xls')
 cur.close()
