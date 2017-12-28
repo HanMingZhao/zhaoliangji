180f21053ffbc8e595a4e4e229299697db111184
ming <591177@qq.com>
2017-12-28 16:20:58 +0800

    fixredishost

--- viewdetals.py
+++ viewdetals.py
@@ -2,15 +2,17 @@ import redis
 import config as conf
 import pymysql as db
 import xlwt
+import time
 
+start = time.time()
 cf = conf.product
 conn = db.connect(host=cf['host'], user=cf['user'], passwd=cf['pass'], db=cf['db'], port=cf['port'], charset=conf.char)
 cur = conn.cursor()
-pool = redis.ConnectionPool(host='118.190.149.162', port=36379)
+pool = redis.ConnectionPool(host='127.0.0.1', port=36379)
 r = redis.StrictRedis(connection_pool=pool, decode_responses=True)
 workbook = xlwt.Workbook()
 prefix = 'count_product_detail_key:'
-
+print('2016sqlscan...', time.time()-start)
 product_sql = '''
 SELECT oo.product_id,pp.product_name,date(oo.pay_at) FROM panda.`odi_order` oo
 left join panda.pdi_product pp 
@@ -27,12 +29,13 @@ sheet.write(0, 0, '销售日期')
 sheet.write(0, 1, '产品ID')
 sheet.write(0, 2, '产品名称')
 sheet.write(0, 3, '访问次数')
+print('2016redisscan...', time.time()-start)
 for i, res in enumerate(result):
     sheet.write(i+1, 0, str(res[2]))
     sheet.write(i+1, 1, r[0])
     sheet.write(i+1, 2, r[1])
     sheet.write(i+1, 3, r.get(prefix+str(r[0])))
-
+print('2017sqlscan...', time.time()-start)
 cur.execute(product_sql.format('2017-1-1', 'date(now())'))
 result = cur.fetchall()
 sheet = workbook.add_sheet('2017')
@@ -40,6 +43,7 @@ sheet.write(0, 0, '销售日期')
 sheet.write(0, 1, '产品ID')
 sheet.write(0, 2, '产品名称')
 sheet.write(0, 3, '访问次数')
+print('2017redisscan...', time.time()-start)
 for i, res in enumerate(result):
     sheet.write(i+1, 0, str(res[2]))
     sheet.write(i+1, 1, r[0])
@@ -49,3 +53,4 @@ for i, res in enumerate(result):
 workbook.save('view.xls')
 cur.close()
 conn.close()
+print('over...', time.time()-start)
