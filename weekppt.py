1715176eda27327bdaa393652cf46ffcc963048e
ming <591177@qq.com>
2018-01-02 20:14:04 +0800

    fixbug

--- weekppt.py
+++ weekppt.py
@@ -88,7 +88,7 @@ AND oo.`pay_at` < '2018-1-1'
 '''
 cur.execute(sales_sql)
 result = cur.fetchall()
-sale_dict = product_count(sales_sql, vd, md, cd, rd)
+sale_dict = product_count(result, vd, md, cd, rd)
 sheet = wb.add_sheet('销量')
 sheet.write(0, 0, '型号')
 sheet.write(0, 1, '内存')
