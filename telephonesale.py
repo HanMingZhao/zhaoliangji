2668df89e3877ce893d7e492ee2d274ebdaa2f91
ming <591177@qq.com>
2018-01-16 19:45:19 +0800

    log

--- telephonesale.py
+++ telephonesale.py
@@ -16,11 +16,12 @@ saled = []
 for phone in dframe['phone']:
     count = conf.product_cursor.execute(two_day_sale_sql.format(conf.yesterday.strftime(conf.date_format),
                                                         conf.tomorrow.strftime(conf.date_format), phone))
+    print(phone)
     if count > 0:
         saled.append(phone)
 sheet = wb.add_sheet('sheet')
 for i, s in enumerate(saled):
-    sheet.write(i, 0, s)
+    sheet.write(i, 0, str(s))
 wb.save(conf.today.strftime(conf.date_format) + 'telesale.xls')
 conf.product_cursor.close()
 conf.product_connect.close()
